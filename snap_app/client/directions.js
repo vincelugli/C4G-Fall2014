530cc44232568b8b2d64ae03f14ef5c3b036e90b
Patrick Conner <pconner03@gmail.com>
2014-11-14 20:19:38 -0500

    Fixed directions loading bug

--- snap_app/client/directions.js
+++ snap_app/client/directions.js
@@ -1,62 +1,59 @@
-if(Meteor.isClient){
+gmap = undefined;
 
-	var mapRendered = false;
 
-	Template.directions.rendered = function(){
-		if(!mapRendered){
-			var fromLat = decodeURIComponent(Router.current().data()["fromLatitude"]);
-			var toLat = decodeURIComponent(Router.current().data()["toLatitude"]);
-			var fromLng = decodeURIComponent(Router.current().data()["fromLongitude"]);
-			var toLng = decodeURIComponent(Router.current().data()["toLongitude"]);
-			var storeName = decodeURIComponent(Router.current().data()["storeName"]);
-			
-			//Do map stuff here
-			GoogleMaps.init(
-				{
-				  'sensor': true, //optional
-				  //key': 'MY-GOOGLEMAPS-API-KEY', //optional
-				  'language': 'en' //optional
-				}, 
-				function(){
-					console.log("Starting map init function");
-					var mapOptions = {
-						zoom: 13,
-						mapTypeId: google.maps.MapTypeId.MAP
-					};
-					gmap = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-					var p = Session.get("pos");
-					var center = new google.maps.LatLng(p.latitude, p.longitude);
-					if(p){
-						gmap.setCenter(center);
-					} 
 
-					directionsDisplay = new google.maps.DirectionsRenderer();
-					directionsService = new google.maps.DirectionsService();
-					var renderOptions = {
-						"preserveViewport": true
-					};
-					directionsDisplay.setOptions(renderOptions);
-					directionsDisplay.setMap(gmap);
-					directionsDisplay.setPanel(document.getElementById("directions-panel"));
+Template.directions.rendered = function(){
 
-					var selectedMode = document.getElementById("mode").value;
-					var request = {
-						origin: new google.maps.LatLng(fromLat, fromLng),
-						destination: new google.maps.LatLng(toLat, toLng),
-						travelMode: selectedMode
-					};
+	var fromLat = decodeURIComponent(Router.current().data()["fromLatitude"]);
+	var toLat = decodeURIComponent(Router.current().data()["toLatitude"]);
+	var fromLng = decodeURIComponent(Router.current().data()["fromLongitude"]);
+	var toLng = decodeURIComponent(Router.current().data()["toLongitude"]);
+	var storeName = decodeURIComponent(Router.current().data()["storeName"]);
+	
+	//Do map stuff here
 
-					directionsService.route(request, function(result, status) {
-						if (status == google.maps.DirectionsStatus.OK) {
-							directionsDisplay.setDirections(result);
-						}
-					});
+
+
+	GoogleMaps.init(
+		{
+		  'sensor': true, //optional
+		  //key': 'MY-GOOGLEMAPS-API-KEY', //optional
+		  'language': 'en' //optional
+		}, 
+		function(){
+			console.log("Starting directions init function");
+			var mapOptions = {
+				zoom: 13,
+				mapTypeId: google.maps.MapTypeId.MAP
+			};
+			gmap = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+			var p = Session.get("pos");
+			var center = new google.maps.LatLng(p.latitude, p.longitude);
+			if(p){
+				gmap.setCenter(center);
+			} 
+
+			directionsDisplay = new google.maps.DirectionsRenderer();
+			directionsService = new google.maps.DirectionsService();
+			var renderOptions = {
+				"preserveViewport": true
+			};
+			directionsDisplay.setOptions(renderOptions);
+			directionsDisplay.setMap(gmap);
+			directionsDisplay.setPanel(document.getElementById("directions-panel"));
+
+			var selectedMode = document.getElementById("mode").value;
+			var request = {
+				origin: new google.maps.LatLng(fromLat, fromLng),
+				destination: new google.maps.LatLng(toLat, toLng),
+				travelMode: selectedMode
+			};
+
+			directionsService.route(request, function(result, status) {
+				if (status == google.maps.DirectionsStatus.OK) {
+					directionsDisplay.setDirections(result);
 				}
-			);
-
-
-
-			mapRendered = true;
+			});
 		}
-	}
+	);
 }
